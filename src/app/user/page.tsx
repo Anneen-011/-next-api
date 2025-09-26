9d1cba4f9127a172fee72850352d68da93df9362
Anneen011 <anneenjunplod@gmailcom>
2025-09-26 17:22:49 +0700

    edited user

---
+++ src/app/user/page.tsx
@@ -0,0 +1,9 @@
+// ตัวอย่าง page.tsx
+export default function UsersPage() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">Users Page</h1>
+      <p>นี่คือหน้า Users</p>
+    </div>
+  );
+}
